4fa58bceef92e30a44ffef7d1dc2467fe9b3b19e
talom45 <164904681+talom45@users.noreply.github.com>
2024-08-26 09:52:34 +0200

    displaying the accountType and Balance

--- src/Bank.java
+++ src/Bank.java
@@ -4,27 +4,26 @@ public class Bank {
     double accountBalance;
 
     Bank(String accountType, Double accountBalance) {
-
         this.accountBalance = accountBalance;
         this.accountType = accountType;
-
     }
-
     public void deposit(double amount) {
         accountBalance += amount;
         System.out.println("Deposited $" + amount + ". New balance: $" + accountBalance);
     }
-
     public void withdraw(double amount) {
         if (amount > accountBalance) {
             System.out.println("Insufficient funds!!");
-        } else {
-            accountBalance -= amount;
-            System.out.println("You Withdrew $" + amount + " from "+accountType+" account"+". New balance: $" + accountBalance);
         }
-
-
+        else {
+            accountBalance -= amount;
+            System.out.println("You Withdrew $" + amount + " from " + accountType + " account" + ". New balance: $" + accountBalance);
+        }
     }
+    public void display(){
+        System.out.println(accountType+" "+ "account");
+        System.out.println("Balance:" + accountBalance);
+        }
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Bank myBank = new Bank("Savings",15.0);
+        Bank myBank = new Bank("Savings", 15.0);
 
-        System.out.println("Account Type:"+myBank.accountType);
+        System.out.println("Account Type:" + myBank.accountType);
         myBank.deposit(500);
-        myBank.withdraw(515);
-
+        myBank.withdraw(55.98);
+        myBank.display();
     }
-    }
+}
